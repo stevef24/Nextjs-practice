3e8d055548f9e45c92c41a41729bd0a1cf9c7a71
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-05-12 21:15:18 +0100

    practice complete

--- app/[searchTerms]/page.tsx
+++ app/[searchTerms]/page.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import getWikiResults from "@/lib/getWikiResults";
+import Item from "./components/Item";
+
 type Props = {
 	params: {
-		searchTerms: string;
+		searchTerm: string;
 	};
 };
 
-export default function page({ params: { searchTerms } }: Props) {
-	return <div>page</div>;
+export async function generateMetaData({ params: { searchTerm } }: Props) {
+	const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+	const data = await wikiData;
+	const displayTerm = searchTerm.replaceAll("%20", "");
+
+	if (!data?.query?.pages) {
+		return {
+			title: `${displayTerm} not found`,
+		};
+	}
+
+	return {
+		title: `${displayTerm} - Wikipedia`,
+		description: `Search results for ${displayTerm} on Wikipedia`,
+	};
+}
+
+export default async function page({ params: { searchTerm } }: Props) {
+	const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+	const data = await wikiData;
+
+	const results: Result[] | undefined = data?.query?.pages;
+	const content = (
+		<main className="max-w-lg min-h-screen py-1 mx-auto bg-slate-200">
+			{results ? (
+				Object.values(results).map((result) => (
+					<Item key={result.pageid} result={result} />
+				))
+			) : (
+				<h2>{`${searchTerm} not found `} </h2>
+			)}
+		</main>
+	);
+	return content;
 }
--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -6,29 +6,28 @@ import { useState, FormEvent } from "react";
 export default function Search() {
 	const [search, setSearch] = useState("");
 	const router = useRouter();
-
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setSearch("");
-		router.push(`/${search}.`);
+		router.push(`/${search}/`);
 	};
 
 	return (
 		<form
 			onSubmit={handleSubmit}
-			className="w-50 flex justify-center gap-2 md:justify-between"
+			className="flex justify-center gap-2 w-50 md:justify-between"
 		>
 			<input
 				type="text"
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 				placeholder="Search Wikipedia"
-				className="bg-white p-2 w-80 text-xl rounded-xl"
+				className="p-2 text-xl text-black bg-white w-80 rounded-xl"
 			/>
 
 			<button
 				type="submit"
-				className="bg-slate-300 p-2 rounded-xl text-white text-xl"
+				className="p-2 text-xl text-white bg-slate-300 rounded-xl"
 			>
 				search
 			</button>
